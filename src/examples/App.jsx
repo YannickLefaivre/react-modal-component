c0d1ee84f67594ecd882fdbda9b3b40b761e0cb3
Yannick LEFAIVRE <debeug@outlook.fr>
2022-08-11 11:18:49 -0400

    build: configure the project to create a react package

---
+++ src/examples/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <>
+      <header>
+        <h1>Headless UI Modal usage examples</h1>
+      </header>
+      <main>
+        <div>Employee successfuly created</div>
+      </main>
+    </>
+  )
+}
+
+export default App
